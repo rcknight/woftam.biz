f1aa812e74bcb2132804743550e3751b5977e439
Rich Knight <rich@rich-knight.com>
2013-09-09 16:57:54 +0100

    test.md

--- add.sh
+++ add.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+EXPECTED_ARGS=2
+E_BADARGS=65
+
+if [ $# -ne $EXPECTED_ARGS ]
+then
+  echo "Usage: `basename $0` {filename} {title}"
+  exit $E_BADARGS
+fi
+
+echo "---" > $1.md;
+echo "layout: \"image\"" >> $1.md;
+echo "title: \"$2\"" >> $1.md;
+echo "imageName: \"$1\"" >> $1.md;
+echo "---" >> $1.md;
+
+cd images
+mogrify -format jpg "$1.gif[0]"
+
---
+++ test.md
@@ -0,0 +1,5 @@
+---
+layout: "image"
+title: "this is the title"
+imageName: "test"
+---
